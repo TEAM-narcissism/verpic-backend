d2d57af45f7538d7900c0faf660d1f50163516cf
kimyoungi99 <kimyoungi99@naver.com>
2021-07-19 21:18:42 +0900

    refact : 유저 리포지토리 관련 코드 단일화
    MemoryUserRepo, 관련 테스트 코드 삭제

--- src/main/java/teamverpic/verpicbackend/repository/UserRepository.java
+++ src/main/java/teamverpic/verpicbackend/repository/UserRepository.java
@@ -2,20 +2,24 @@ package teamverpic.verpicbackend.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import teamverpic.verpicbackend.domain.User;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository {
-    User save(User user);
-    Optional<User> findById(Long id);
-    List<User> findAllByFirstName(String firstName);
-    List<User> findAllByLastName(String lastName);
+public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findByEmail(String email);
 
-    // 유저 검색
+    List<User> findAllByFirstName(String firstName);
+
+    List<User> findAllByLastName(String lastName);
+
+    // 유저 검색을 위한 함수
     List<User> findAllByFirstNameContaining(String partOfFirstName);
+
     List<User> findAllByLastNameContaining(String partOfLastName);
+
     List<User> findAllByEmailContaining(String partOfEmail);
 }
