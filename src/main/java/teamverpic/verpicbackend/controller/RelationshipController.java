07ad6300ad37232de958633fcda7acbd4f33934d
Youhoseong <h0song@naver.com>
2021-07-28 15:15:26 +0900

    refactor: friend request code refactoring

--- src/main/java/teamverpic/verpicbackend/controller/RelationshipController.java
+++ src/main/java/teamverpic/verpicbackend/controller/RelationshipController.java
@@ -23,48 +23,14 @@ public class RelationshipController {
     private final RelationshipService relationshipService;
     private final UserRepository userRepository;
 
-    @ExceptionHandler
-    public ResponseEntity<HttpResponseDto> customNullPointerExceptionHandler(CustomNullPointerException e){
-        return new ResponseEntity<>(e.getResponseDto(), e.getHttpHeaders(), e.getResponseDto().getHttpStatus());
-    }
 
 
     // 친구 요청
     @PostMapping("/{receiverId}")
-    public ResponseEntity<HttpResponseDto> relationshipRequest(Authentication authentication, @PathVariable Long receiverId) {
-        HttpHeaders headers= new HttpHeaders();
-        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
-
-        HttpResponseDto responseDto = new HttpResponseDto();
-
-        responseDto.setHttpStatus(HttpStatus.BAD_REQUEST);
-        responseDto.setMessage("현재 로그인한 유저를 찾을 수 없어요.");
-
-        HttpResponseDto finalResponseDto = responseDto;
-        User currentLoginUser = userRepository.findByEmail(authentication.getName())
-                .orElseThrow(()->
-                        new CustomNullPointerException(finalResponseDto, headers));
-
-        responseDto.setMessage("친구 요청을 받을 유저를 찾을 수 없어요.");
-        HttpResponseDto finalResponseDto1 = responseDto;
-        User receiver = userRepository.findById(receiverId)
-                .orElseThrow(()->
-                        new CustomNullPointerException(finalResponseDto1, headers));
-
-        System.out.println();
-        System.out.println("####################### 친구 요청 api 실행 #######################");
-        System.out.println("currentLoginUser.getEmail() = " + currentLoginUser.getEmail());
-        System.out.println("################################################################");
-        System.out.println();
-
-        responseDto = relationshipService.relationshipRequest(currentLoginUser, receiver);
-        if(responseDto.getHttpStatus() != HttpStatus.CREATED) {
-            return new ResponseEntity<>(responseDto, headers, responseDto.getHttpStatus());
-        } else {
-            responseDto.setMessage(receiver.getFirstName() + receiver.getLastName() + " 님에게 친구 요청을 보냈어요.");
-            responseDto.setHttpStatus(HttpStatus.CREATED);
-            return new ResponseEntity<>(responseDto, headers, HttpStatus.CREATED);
-        }
+    public String relationshipRequest(Authentication authentication, @PathVariable Long receiverId) {
+        String userEmail = authentication.getName();
+        relationshipService.relationshipRequest(userEmail, receiverId);
+        return "{ id: " +receiverId + " } 님에게 친구 요청을 보냈어요.";
     }
 
     // 친구 수락
