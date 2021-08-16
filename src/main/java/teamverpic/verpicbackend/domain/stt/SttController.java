effef6a5d0f476c17e7853fc7c770d6e6de5e72a
rlacksgus97 <rlacksgus97@naver.com>
2021-08-16 18:43:22 +0900

    feat: feeback domain created

--- src/main/java/teamverpic/verpicbackend/domain/stt/SttController.java
+++ src/main/java/teamverpic/verpicbackend/domain/stt/SttController.java
@@ -1,42 +1,42 @@
-package teamverpic.verpicbackend.domain.stt;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RequiredArgsConstructor
-@RestController
-public class SttController {
-
-    private final SttService sttService;
-
-    @GetMapping("/stt")
-    public void example() throws Exception {
-        try{
-            System.out.println("SttController.stt");
-            sttService.example();
-        }catch (Exception e) {
-            System.out.println("SttController.stt fail");
-            e.printStackTrace();
-        }
-    }
-
-    @GetMapping("/longstt")
-    public void asyncRecognizeGcs() throws Exception{
-        try{
-            String gcsUri = "gs://verpic-speech-record/녹음.flac";
-            sttService.asyncRecognizeGcs(gcsUri);
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-//    @GetMapping("/streaming")
-//    public void streaming() throws Exception {
+//package teamverpic.verpicbackend.domain.stt;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RequiredArgsConstructor
+//@RestController
+//public class SttController {
+//
+//    private final SttService sttService;
+//
+//    @GetMapping("/stt")
+//    public void example() throws Exception {
 //        try{
-//            sttService.streamingMicRecognize();
-//        } catch (Exception e) {
+//            System.out.println("SttController.stt");
+//            sttService.example();
+//        }catch (Exception e) {
+//            System.out.println("SttController.stt fail");
 //            e.printStackTrace();
 //        }
 //    }
-}
+//
+//    @GetMapping("/longstt")
+//    public void asyncRecognizeGcs() throws Exception{
+//        try{
+//            String gcsUri = "gs://verpic-speech-record/녹음.flac";
+//            sttService.asyncRecognizeGcs(gcsUri);
+//        }catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+////    @GetMapping("/streaming")
+////    public void streaming() throws Exception {
+////        try{
+////            sttService.streamingMicRecognize();
+////        } catch (Exception e) {
+////            e.printStackTrace();
+////        }
+////    }
+//}
