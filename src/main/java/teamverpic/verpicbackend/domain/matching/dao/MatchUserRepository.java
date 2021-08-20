bca3652cce20f953dcfbfac913da6975ffafc040
heoboseong7 <boseong844@gmail.com>
2021-08-20 21:01:26 +0900

    refactor: videochat refactoring

--- src/main/java/teamverpic/verpicbackend/domain/matching/dao/MatchUserRepository.java
+++ src/main/java/teamverpic/verpicbackend/domain/matching/dao/MatchUserRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import teamverpic.verpicbackend.domain.matching.domain.MatchUser;
 
 public interface MatchUserRepository extends JpaRepository<MatchUser, Long> {
+    MatchUser findByUserIdAndMatchId(Long userId, Long matchId);
 }
