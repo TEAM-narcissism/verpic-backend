35b56ae31a44a923ff0cf709f14b9ecf1a2c4491
Tae Hun Kim <zaqxsw1397@gmail.com>
2021-08-25 18:03:19 +0900

    fix: modify user profile update

--- src/main/java/teamverpic/verpicbackend/domain/user/dto/UserUpdateRequestDto.java
+++ src/main/java/teamverpic/verpicbackend/domain/user/dto/UserUpdateRequestDto.java
@@ -2,11 +2,18 @@ package teamverpic.verpicbackend.domain.user.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Getter
+@NoArgsConstructor
 public class UserUpdateRequestDto {
 
     // 프로필 설정 시 추가 (detail_profile)
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
     private String firstLanguage;
     private String learnLanguage;
 //    private String availableLanguage;
@@ -14,7 +21,11 @@ public class UserUpdateRequestDto {
 //    private String interest;
 
     @Builder
-    public UserUpdateRequestDto(String firstLanguage, String learnLanguage) {
+    public UserUpdateRequestDto(String firstName, String lastName, Date birthDate,
+                                String firstLanguage, String learnLanguage) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
         this.firstLanguage = firstLanguage;
         this.learnLanguage = learnLanguage;
     }
