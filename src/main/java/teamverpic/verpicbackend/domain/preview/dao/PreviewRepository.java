f8809803f10b7b2af94fb656e5a1b27c4cc93c50
Youhoseong <h0song@naver.com>
2021-08-29 19:41:12 +0900

    feat: 미리보기 dto에 토픽도 추가함

--- src/main/java/teamverpic/verpicbackend/domain/preview/dao/PreviewRepository.java
+++ src/main/java/teamverpic/verpicbackend/domain/preview/dao/PreviewRepository.java
@@ -1,7 +1,14 @@
 package teamverpic.verpicbackend.domain.preview.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import teamverpic.verpicbackend.domain.preview.domain.Preview;
 
+import java.util.Optional;
+
 public interface PreviewRepository extends JpaRepository<Preview, Long> {
+
+
+    Optional<Preview> findById(Long id);
 }
