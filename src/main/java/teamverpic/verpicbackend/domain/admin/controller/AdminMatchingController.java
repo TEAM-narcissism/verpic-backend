ab03db72f6301cfb4b6811fd5af0121c955461d1
heoboseong7 <boseong844@gmail.com>
2021-09-06 22:41:48 +0900

    feat: topidId로 매칭 기능 추가

---
+++ src/main/java/teamverpic/verpicbackend/domain/admin/controller/AdminMatchingController.java
@@ -0,0 +1,27 @@
+package teamverpic.verpicbackend.domain.admin.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import teamverpic.verpicbackend.domain.matching.service.MatchService;
+import teamverpic.verpicbackend.domain.reservation.dao.StudyReservationRepository;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/administration/matching")
+public class AdminMatchingController {
+
+    private final StudyReservationRepository studyReservationRepository;
+    private final MatchService matchService;
+
+    @PostMapping("/{topicId}")
+    @ResponseBody
+    public String startMatching(@PathVariable Long topicId) {
+        int[] startTimes = {17, 18, 19};
+        for (int startTime : startTimes) {
+            matchService.match(studyReservationRepository.findAllByTopicIdAndStartTime(topicId, startTime));
+        }
+
+        return "매칭 성공";
+    }
+}
